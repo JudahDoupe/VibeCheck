273a187c87f4cf12b2ed30235d4b0ac2a5fed622
judahdoupe <judahdoupe@gmail.com>
2025-01-26 17:41:33 -0500

    tried drawing svg paths for the feelings

--- VibeCheck/obj/Debug/net9.0/VibeCheck.AssemblyInfo.cs
+++ VibeCheck/obj/Debug/net9.0/VibeCheck.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VibeCheck")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33c71fbb40449eeaf8757bf60529dcdc0adbdad1")]
 [assembly: System.Reflection.AssemblyProductAttribute("VibeCheck")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VibeCheck")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
