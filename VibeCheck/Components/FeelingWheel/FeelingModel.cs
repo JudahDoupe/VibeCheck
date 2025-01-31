496856bfbbb16544126c5f15382074db4ca41a2e
judahdoupe <judahdoupe@gmail.com>
2025-01-30 19:30:31 -0500

    imporved text position a little

--- VibeCheck/Components/FeelingWheel/FeelingModel.cs
+++ VibeCheck/Components/FeelingWheel/FeelingModel.cs
@@ -4,32 +4,7 @@ public record FeelingModel(string Name, string? Parent);
 
 public record FeelingTileModel(Angle Start, Angle End, double InnerRadius, double OuterRadius, string Color)
 {
-    
-    public string GeneratePath()
-    {
-        var innerStart =
-            $"{OuterRadius + InnerRadius * Math.Sin(Start.Radians)},{OuterRadius - InnerRadius * Math.Cos(Start.Radians)}";
-        var outerStart =
-            $"{OuterRadius + OuterRadius * Math.Sin(Start.Radians)},{OuterRadius - OuterRadius * Math.Cos(Start.Radians)}";
-        
-        var innerEnd =
-            $"{OuterRadius + InnerRadius * Math.Sin(End.Radians)},{OuterRadius - InnerRadius * Math.Cos(End.Radians)}";
-        var outerEnd =
-            $"{OuterRadius + OuterRadius * Math.Sin(End.Radians)},{OuterRadius - OuterRadius * Math.Cos(End.Radians)}";
-        
-        
-        var innerSvgRadius = $"{InnerRadius},{InnerRadius}";
-        var outerSvgRadius = $"{OuterRadius},{OuterRadius}";
-
-        var path = $"M{innerStart} ";
-        path += $"L{outerStart} ";
-        path += $"A{outerSvgRadius} 1 0 1 {outerEnd} ";
-        path += $"L{innerEnd} ";
-        path += $"A{innerSvgRadius} 1 0 0 {innerStart} ";
-        path += "z";
-        
-        return path;
-    }
+    public double Length => OuterRadius - InnerRadius;
 }
 
 public record Angle
@@ -46,5 +21,17 @@ public record Angle
     public static Angle FromRatio(double ratio) => new(ratio);
     public static Angle FromDregrees(double degrees) => new(degrees / 180.0);
     public static Angle FromRadians(double radians) => new(radians / (2 * Math.PI));
+
+    public Point2D Project(double distance) => new Point2D(distance * Math.Sin(Radians), distance * Math.Cos(Radians));
     
+    public Angle Inverse() => FromRatio(1-Ratio);
+
 }
+
+public record Point2D(double X, double Y)
+{
+    public string ToSvg() => $"{X},{Y}";
+    
+    public Point2D Add(Point2D other) => new(X + other.X, Y + other.Y); 
+    public Point2D Add(double x, double y) => new(X + x, Y + y); 
+};
